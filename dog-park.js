becea3de30708f36d156a15c1ddbcbfc0bc23afe
Carly Clift <carlymclift@gmail.com>
2020-04-20 19:08:19 -0600

    Complete

--- dog-park.js
+++ dog-park.js
@@ -1,5 +1,14 @@
 class DogPark {
-
+  constructor(name) {
+    this.name = name;
+    this.dogs = [];
+  }
+  addDog(sophie) {
+    this.dogs.push(sophie)
+  }
+  removeDog(toph) {
+    this.dogs.splice(1, 1)
+  }
 }
 
 module.exports = DogPark;
--- dog.js
+++ dog.js
@@ -1,5 +1,53 @@
 class Dog {
-
+  constructor(dogInfo) {
+    this.name = dogInfo.name;
+    this.breed = dogInfo.breed
+    this.energy = 3;
+    this.hungry = false;
+    this.toys = [];
+    this.squeakyToys = [];
+    this.friends = [];
+    this.playfulFriends = [];
+    this.inDogPark = false;
+  }
+  getToy(toys) {
+    this.toys.push(toys);
+  }
+  findSqueakyToys() {
+    for (var i = 0; i < this.toys.length; i++) {
+      if (this.toys[i].squeaky === true) {
+        this.squeakyToys.push(this.toys[i]);
+      }
+    }
+    return this.squeakyToys
+  }
+  play(){
+    this.energy--;
+    if (this.energy <= 1) {
+      this.hungry = true;
+    }
+  }
+  eat() {
+    this.hungry = false;
+    return `I'm not ready to eat!`
+  }
+  makeFriends(friend) {
+    this.friends.push(friend)
+  }
+  findFriendsToPlayWith() {
+    for (var i = 0; i < this.friends.length; i++) {
+      if (this.friends[i].energy === 3) {
+        this.playfulFriends.push(this.friends[i])
+      }
+    }
+    return this.playfulFriends
+  }
+  enterDogPark(cherryCreek) {
+    this.inDogPark = true;
+  }
+  leaveDogPark(cherryCreek) {
+    this.inDogPark = false;
+  }
 }
 
 module.exports = Dog;
